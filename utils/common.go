e58ba5576f175a9d22d0d86ddef8af02ab4479b5
keimoon <keimoonvie@gmail.com>
2018-06-25 18:03:42 +0700

    Pump version to 1.0.1

--- utils/common.go
+++ utils/common.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version of rivendell
-var Version = "1.0.0"
+var Version = "1.0.1"
 
 // MergeMaps merges multiple maps into one
 func MergeMaps(maps ...map[string]string) map[string]string {
